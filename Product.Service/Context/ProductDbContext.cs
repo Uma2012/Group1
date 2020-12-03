dd1d348aa78f9f87a389f71cf98ea14b3e7082c4
uma Prithi <umaprithi@outlook.com>
2020-12-03 14:59:06 +0100

    Created Dbset product (Not completed)

---
+++ Product.Service/Context/ProductDbContext.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Product.Service.Models;
+
+
+namespace Product.Service.Context
+{
+    public class ProductDbContext: DbContext
+    {
+       
+        public ProductDbContext(DbContextOptions<ProductDbContext> options):base(options)
+        {
+
+        }
+
+        public DbSet<Models.Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<Models.Product>(entity =>
+            {
+                entity.Property(x => x.Name).IsRequired();
+                entity.Property(x => x.Price).IsRequired();
+                entity.Property(x => x.Quantity).IsRequired();
+                entity.Property(x => x.ImageUrl);
+                entity.Property(x => x.Description).IsRequired();
+
+            });
+            builder.Entity<Models.Product>().HasData(
+                new Models.Product() {Name="Test",Price=200,Quantity=15,Description="Description" }
+
+                );
+        }
+
+        }
+}
--- Product.Service/Startup.cs
+++ Product.Service/Startup.cs
@@ -2,10 +2,13 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using Product.Service.Context;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -26,6 +29,9 @@ namespace Product.Service
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            //services.AddDbContext<ProductDbContext>(options =>
+            //  options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
