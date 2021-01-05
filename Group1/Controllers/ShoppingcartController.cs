760add7752a67b3adeaef368c2b046bbcefefefc
Josefine <josefine@anerland.se>
2021-01-05 14:10:43 +0100

    Added controller, view + button for shoppingcart

---
+++ Group1/Controllers/ShoppingcartController.cs
@@ -0,0 +1,17 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Group1.Web.Controllers
+{
+    public class ShoppingcartController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
