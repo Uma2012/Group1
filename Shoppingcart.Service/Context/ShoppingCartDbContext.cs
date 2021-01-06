75f3dd5966a9058daf6d3a012db5e702aca49c5a
uma Prithi <umaprithi@outlook.com>
2021-01-06 19:54:03 +0100

    Created shoppingcart service and Test projects. Wrote connection string in appsettings and invoked in startup class

---
+++ Shoppingcart.Service/Context/ShoppingCartDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shoppingcart.Service.Context
+{
+    public class ShoppingCartDbContext :DbContext
+    {
+        public ShoppingCartDbContext(DbContextOptions<ShoppingCartDbContext> options):base(options)
+        {
+
+        }
+    }
+}
---
+++ Shoppingcart.Test/ControllerTest.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shoppingcart.Test
+{
+    class ControllerTest
+    {
+    }
+}
