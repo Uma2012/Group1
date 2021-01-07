db071dc8d315611149166a61f1f31dd354cf6b1f
uma Prithi <umaprithi@outlook.com>
2021-01-07 10:06:50 +0100

    Added addtocart, getcartcontent and deletecartcontent methods with session cookies

---
+++ Group1/Views/Shoppingcart/GetCartContent.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Group1.Web.Views.Shoppingcart
+{
+    public class GetCartContent
+    {
+    }
+}
