5aa1e99a0e243319f1ea105c4e1930d01e8c6eb2
Danial <danial97rhz@outlook.com>
2021-01-18 12:24:23 +0100

    Added total price in shopping cart

--- Group1/wwwroot/js/checkFee.js
+++ Group1/wwwroot/js/checkFee.js
@@ -2,6 +2,8 @@
 {
     var homedeliveryFee = document.querySelector('input[name="deliveryMethod"]:checked').value;
     var fee = 0;
+    var subtotal = $(this).attr("TotalPrice").value;
+
     if (homedeliveryFee == 1) {
         fee = 50;
     }
@@ -11,4 +13,6 @@
 
     document.getElementById('shippingFee').value = fee;
     document.getElementById('shippingFee').innerText = document.getElementById('shippingFee').value + " SEK";
+    var totalPrice = +subtotal + +fee;
+    document.getElementById('totalPrice').innerText = totalPrice + " SEK"
 }
