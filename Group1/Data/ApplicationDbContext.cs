3dcef0ae1dea268bb7b4e40c7b7a6795b2269868
JohanWallner <wallner.johan@gmail.com>
2021-01-20 09:42:14 +0100

    Revert "Product tests"

--- Group1/Data/ApplicationDbContext.cs
+++ Group1/Data/ApplicationDbContext.cs
@@ -1,5 +1,4 @@
-﻿using Group1.Web.Models;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -7,7 +6,7 @@ using System.Text;
 
 namespace Group1.Data
 {
-    public class ApplicationDbContext : IdentityDbContext<CustomUser>
+    public class ApplicationDbContext : IdentityDbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
